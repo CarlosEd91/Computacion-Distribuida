460dc00be50d6f7d2710750730f9389e887e5411
CarlosEd91 <carlos.e.sampedro.91@gmail.com>
2015-12-18 19:37:48 -0500

    C

--- EleccionLider/Lider.c
+++ EleccionLider/Lider.c
@@ -18,7 +18,6 @@ int main(int argc, char* argv[])
   	MPI_Comm_size(MPI_COMM_WORLD,&numero_procesos); 
   	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	
-		
 	srand(time(0)+rank);  
 	numero=rand()% 37;
 
